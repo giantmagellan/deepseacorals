ecb4dcec8ca6a13c59970c6a5162574d09b8aa41
bryan <bryanflores@sandiego.edu>
2023-07-02 14:35:06 -0700

    impute method: exponential moving avg

--- scripts/ingestion.py
+++ scripts/ingestion.py
@@ -7,17 +7,19 @@ import glob
 path = os.path.abspath("./data_raw/")
 csv_files = glob.glob(os.path.join(path, "*.csv"))
 
-def import_coral_csv(csv_name):
+# TBD: Add errors and logging
+
+def import_coral_csv(csv):
     """
     Import CSVs and transform column names into a standardized format.
-    :param csv_name: str, CSV file name
+    :param csv: path, relative path of CSV
     :return df: reformatted Pandas dataframe
     """
     # Read csv and remove index
-    df = pd.read_csv(csv_name, index_col=False)
+    df = pd.read_csv(csv, index_col=False)
 
     # Convert column names to lower case
-    df.columns.str.strip().str.lower()
+    df.columns = df.columns.str.strip().str.lower()
 
     return df
 
@@ -30,4 +32,14 @@ def transform_imports(df):
     # Convert time column to date format: YYYY-MM-DD
     df['time'] = pd.to_datetime(df['time']).dt.date
     df = df.rename(columns={'time': 'date'}, errors='raise')
+    # Change float to int dtype
+    df['crw_dhw'] = df['crw_dhw'].astype('int64')
+
+    # For columns w/ missing values, impute values w/ exponential moving avg
+    for col in df.columns:
+        if df[col].isnull().sum() > 0:
+            df[col] = df[col].fillna(df[col].ewm(span=10).mean())
+        else:
+            pass
     
+    return df
